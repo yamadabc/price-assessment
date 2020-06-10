ee0f33ca7aeeb3b8d78ff5e8afa6f2367e301746
yamadabc <57384471+yamadabc@users.noreply.github.com>
2020-06-10 16:13:11 +0900

    価格をnullableに変更

--- database/migrations/2020_05_21_152954_create_rooms_table.php
+++ database/migrations/2020_05_21_152954_create_rooms_table.php
@@ -20,16 +20,16 @@ class CreateRoomsTable extends Migration
             $table->smallInteger('floor_number')->nullable()->index();
             $table->char('layout',50)->nullable();
             $table->char('direction',50)->nullable();
-            $table->float('occupied_area',6,2)->default(0);
+            $table->float('occupied_area',6,2)->nullable();
             $table->float('balcony_area',6,2)->default(0);
             $table->float('roof_balcony_area',6,2)->default(0);
             $table->float('terass_area',6,2)->default(0);
             $table->float('garden_area',6,2)->default(0);
             $table->char('residential_building_name',100)->nullable();//棟名
             $table->char('layout_type',100)->nullable()->index();
-            $table->float('published_price',8,2)->default(0);//新築時売買価格
-            $table->float('expected_price',8,2)->default(0);//予想売買価格
-            $table->float('expected_rent_price',10,2)->default(0);//予想賃料
+            $table->float('published_price',8,2)->nullable();//新築時売買価格
+            $table->float('expected_price',8,2)->nullable();//予想売買価格
+            $table->float('expected_rent_price',10,2)->nullable();//予想賃料
             $table->boolean('has_no_data')->default(0);
             $table->timestamps();
 
