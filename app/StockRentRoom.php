06e3b76ec5ba76a89afa9b8a116ad6cab7b34815
yamadabc <57384471+yamadabc@users.noreply.github.com>
2020-08-25 11:49:18 +0900

    nullsubzero関数作成

--- app/StockRentRoom.php
+++ app/StockRentRoom.php
@@ -13,5 +13,49 @@ class StockRentRoom extends Model
         return $this->belongsTo('App\Room');
     }
 
-    
+    /**
+     * nullなら0を代入
+     * @param Request $request
+     * @return int[]
+     */
+    public function nullSubZero($request)
+    {
+        if($request->previous_price === null){
+            $previous_price = 0;
+        }else{
+            $previous_price = $request->previous_price;
+        }
+
+        if($request->management_fee === null){
+            $management_fee = 0;
+        }else{
+            $management_fee = $request->management_fee;
+        }
+
+        if($request->monthly_fee === null){
+            $monthly_fee = 0;
+        }else{
+            $monthly_fee = $request->monthly_fee;
+        }
+
+        if($request->security_deposit === null){
+            $security_deposit = 0;
+        }else{
+            $security_deposit = $request->security_deposit;
+        }
+
+        if($request->gratuity_fee === null){
+            $gratuity_fee = 0;
+        }else{
+            $gratuity_fee = $request->gratuity_fee;
+        }
+
+        if($request->deposit === null){
+            $deposit = 0;
+        }else{
+            $deposit = $request->deposit;
+        }
+
+        return ['previous_price' => $previous_price ,'management_fee' => $management_fee,'monthly_fee' => $monthly_fee,'security_deposit' => $security_deposit ,'gratuity_fee' => $gratuity_fee,'deposit' => $deposit];
+    }
 }
